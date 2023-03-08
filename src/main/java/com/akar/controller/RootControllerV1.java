3bfeafb6cffcb650f5c4725a014ae9a835e52e3e
Min97 <akar97@lab2ai.com>
2023-03-08 10:43:34 +0900

    엔티티 변경

--- src/main/java/com/akar/controller/RootControllerV1.java
+++ src/main/java/com/akar/controller/RootControllerV1.java
@@ -1,6 +1,6 @@
 package com.akar.controller;
 
-import com.akar.entity.UserEntity;
+import com.akar.entity.UserEntityV1;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class RootControllerV1 {
     @GetMapping("/v1/test")
     public String returnTestString(){
-        UserEntity userEntity = UserEntity.builder()
+        UserEntityV1 userEntity = UserEntityV1.builder()
                 .name("test1")
                 .build();
 
